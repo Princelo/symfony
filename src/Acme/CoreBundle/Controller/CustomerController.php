acfa6bf7e4df0048ebd38a4400c103436b9d258b
Princelo <lamkimcheung@gmail.com>
2015-05-14 02:52:59 +0800

    log back to 1

--- src/Acme/CoreBundle/Controller/CustomerController.php
+++ src/Acme/CoreBundle/Controller/CustomerController.php
@@ -26,7 +26,7 @@ class CustomerController extends Controller implements InitializableControllerIn
         //generate should_rank_log
         $timeNow = new \DateTime('now');
         $intWeek = date('w', $timeNow->getTimestamp());
-        $floatShouldRank = $this->datediffInWeeks('5/10/2015', $timeNow->format('m/d/Y'));
+        $floatShouldRank = $this->datediffInWeeks('5/17/2015', $timeNow->format('m/d/Y'));
         //get latest rank log
         $intCountRanked = $objORM->getRepository('AcmeBackendBundle:RankLog')
             ->getIntCountRanked();
@@ -38,8 +38,8 @@ class CustomerController extends Controller implements InitializableControllerIn
         $session->set('rank_week_day', $intRankWeekDay);
         $intNextRankTime = $this->getIntNextRankTime($intRankWeekDay);
         $session->set('next_rank_time', $intNextRankTime);
-        //if($intWeek >= $intRankWeekDay)
-            //$floatShouldRank += 1;
+        if($intWeek >= $intRankWeekDay)
+            $floatShouldRank += 1;
         //if should_rank_log's count > rank_log's count
         //generate rank
         if($floatShouldRank > $intCountRanked){
